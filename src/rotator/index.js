43a2ab3c33d00c71bb9a60c57141a4180f4073e9
Pedro Mass <pedro.j.mass@gmail.com>
2018-04-26 15:33:12 -0400

    Get the letters rotating

--- src/rotator/index.js
+++ src/rotator/index.js
@@ -17,7 +17,6 @@ class TextRotator extends Component {
   };
 
   state = {
-    previousIndex: undefined,
     currentIndex: 0,
   };
 
@@ -27,7 +26,7 @@ class TextRotator extends Component {
     return (
       <div className={classnames("text", this.props.className)}>
         <p>Your personal </p>
-        <p>{this.renderWords(this.props.words)}</p>
+        <p>{this.renderWords(this.props.words, this.state.currentIndex)}</p>
       </div>
     );
   }
@@ -41,48 +40,129 @@ class TextRotator extends Component {
   }
 
   changeWord = () => {
-    let { previousIndex, currentIndex } = this.state;
+    let { currentIndex } = this.state;
     const maxLength = this.props.words.length;
-    previousIndex = (previousIndex + 1) % maxLength;
     currentIndex = (currentIndex + 1) % maxLength;
 
     this.setState({
-      previousIndex,
       currentIndex,
     });
   };
 
-  renderWords(words = []) {
+  renderWords(words = [], currentIndex = 0) {
+    const previousIndex = (currentIndex - 1) % words.length;
     return words.map((word, index) => {
       return (
         <span
           className={classnames("word", {
-            "current-word": index === this.state.currentIndex,
+            "current-word": index === currentIndex,
           })}
           key={index}
         >
-          {this.renderWord(word, index)}
+          {this.renderWord(word, index, currentIndex, previousIndex)}
         </span>
       );
     });
   }
 
-  renderWord(word = "", wordIndex) {
-    const { previousIndex, currentIndex } = this.state;
+  renderWord(word = "", wordIndex, currentIndex, previousIndex) {
     const letterClass =
       wordIndex === previousIndex
         ? "out"
         : wordIndex === currentIndex
           ? "in"
-          : "behind";
+          : undefined;
+    console.log("letterClass: ", letterClass);
+    const things = {
+      out: {
+        animationClass: "out",
+        getDelay: index => index * 80,
+      },
+      in: {
+        animationClass: "in",
+        getDelay: index => 340 + index * 80,
+        className: "behind",
+      },
+      default: {
+        animationClass: "out",
+        getDelay: () => 0,
+      },
+    };
+
+    const letterInfo =
+      wordIndex === previousIndex
+        ? things.out
+        : wordIndex === currentIndex
+          ? things.in
+          : things.default;
 
     return word.split("").map((letter, index) => {
       return (
-        <span className={classnames("letter", letterClass)} key={index}>
-          {letter}
-        </span>
+        <Letter
+          key={index}
+          letter={letter}
+          delay={letterInfo.getDelay(index)}
+          className={letterInfo.className}
+          animationClass={letterInfo.animationClass}
+        />
+
+        // <span className={classnames("letter", letterClass)} key={index}>
+        //   {letter}
+        // </span>
       );
     });
+  }
+}
+
+class Letter extends Component {
+  static displayName = "Letter";
+
+  static propTypes = {
+    letter: PropTypes.string.isRequired,
+    className: PropTypes.string,
+    animationClass: PropTypes.string,
+    delay: PropTypes.number,
+  };
+
+  state = {
+    animationClass: undefined,
+  };
+
+  render() {
+    const className = this.state.animationClass
+      ? this.state.animationClass
+      : this.props.className;
+
+    return (
+      <span className={classnames("letter", className)}>
+        {this.props.letter}
+      </span>
+    );
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (this.props.className !== prevProps.className) {
+      this.updateClassName(this.props);
+    }
+  }
+
+  componentDidMount() {
+    this.updateClassName(this.props);
+    // setTimeout(() => {
+    //   console.log("applying class: ", this.props.className);
+    //   this.setState({
+    //     animationClass: this.props.className,
+    //   });
+    // }, this.props.delay);
+  }
+
+  updateClassName(props = this.props) {
+    setTimeout(() => {
+      console.log("applying animationClass: ", props.animationClass);
+      this.setState({
+        animationClass: props.animationClass,
+      });
+    }, props.delay);
   }
 }
 
